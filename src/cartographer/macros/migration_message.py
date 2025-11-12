35430b30e7abafc103f8a39b31efdd4a79a0ffc7
Jonas Damtoft <Jomik@users.noreply.github.com>
2025-11-12 20:13:08 +0100

    fix: update message for old macros

--- src/cartographer/macros/migration_message.py
+++ src/cartographer/macros/migration_message.py
@@ -9,13 +9,10 @@ from cartographer.interfaces.printer import Macro, MacroParams
 
 @final
 class MigrationMessageMacro(Macro):
-    description = "Implements an old macro and informs of the new"
-
     def __init__(self, name: str, new_macro: str) -> None:
-        self.name = name
-        self.new_macro = new_macro
+        self.description = f"Macro {name} has been replaced by {new_macro}."
 
     @override
     def run(self, params: MacroParams) -> None:
-        msg = f"Macro {self.name} is deprecated and replaced by {self.new_macro}. Please update your configuration."
+        msg = f"{self.description} Please update your configuration."
         raise RuntimeError(msg)
