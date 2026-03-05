6fb240a67911962806a80e6076ca15cb9db0dd87
Jonas Damtoft <Jomik@users.noreply.github.com>
2026-03-05 17:39:22 +0100

    fix(touch): correctly format nozzle temp (#460)

--- src/cartographer/probe/touch_mode.py
+++ src/cartographer/probe/touch_mode.py
@@ -299,7 +299,7 @@ class TouchMode(TouchModelSelectorMixin, ProbeMode, Endstop):
         nozzle_temperature = max(self._toolhead.get_extruder_temperature())
         max_temp = self._config.max_touch_temperature
         if nozzle_temperature > max_temp + MAX_TOUCH_TEMPERATURE_EPSILON:
-            msg = f"Nozzle temperature must be below {max_temp:d}C, was {nozzle_temperature:d}C"
+            msg = f"Nozzle temperature must be below {max_temp:d}C, was {nozzle_temperature:.1f}C"
             raise RuntimeError(msg)
         return self._mcu.start_homing_touch(print_time, model.threshold)
 
